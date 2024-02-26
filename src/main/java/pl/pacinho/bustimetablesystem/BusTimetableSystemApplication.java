c13a9ed638a432bd440c56ab0996201d4f118d3e
patryk_ojdana <patryk.ojdana@axfina.pl>
2024-02-26 14:36:26 +0100

    Init

---
+++ src/main/java/pl/pacinho/bustimetablesystem/BusTimetableSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.pacinho.bustimetablesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusTimetableSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusTimetableSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pacinho/bustimetablesystem/BusTimetableSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pacinho.bustimetablesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusTimetableSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
