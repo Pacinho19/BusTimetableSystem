72abf3dc4f99800843043cc14472839f5fd9591d
patryk_ojdana <patryk.ojdana@axfina.pl>
2024-02-27 10:19:24 +0100

    Add tests for BusStopTools Add NonNull annotation to method findFirstStopOnTheRoute and findLastStopOnTheRoute params

--- src/main/java/pl/pacinho/bustimetablesystem/bus/tools/BusStopTools.java
+++ src/main/java/pl/pacinho/bustimetablesystem/bus/tools/BusStopTools.java
@@ -1,18 +1,19 @@
 package pl.pacinho.bustimetablesystem.bus.tools;
 
+import lombok.NonNull;
 import pl.pacinho.bustimetablesystem.bus.model.entity.BusRouteStop;
 
 import java.util.Comparator;
 import java.util.Set;
 
 public class BusStopTools {
-    public static BusRouteStop findFirstStopOnTheRoute(Set<BusRouteStop> busStops) {
+    public static BusRouteStop findFirstStopOnTheRoute(@NonNull Set<BusRouteStop> busStops) {
         return busStops.stream()
                 .min(Comparator.comparing(BusRouteStop::getNumber))
                 .orElse(null);
     }
 
-    public static BusRouteStop findLastStopOnTheRoute(Set<BusRouteStop> busStops) {
+    public static BusRouteStop findLastStopOnTheRoute(@NonNull Set<BusRouteStop> busStops) {
         return busStops.stream()
                 .max(Comparator.comparing(BusRouteStop::getNumber))
                 .orElse(null);
---
+++ src/test/java/pl/pacinho/bustimetablesystem/bus/tools/BusStopToolsTest.java
@@ -0,0 +1,93 @@
+package pl.pacinho.bustimetablesystem.bus.tools;
+
+import org.junit.jupiter.api.Test;
+import pl.pacinho.bustimetablesystem.bus.model.entity.BusRouteStop;
+
+import java.util.Collections;
+import java.util.Set;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class BusStopToolsTest {
+
+    @Test
+    void nullShouldBeReturnedFromFindFirstStopOnTheRouteWhenRouteStopCollectionIsEmpty() {
+        //given
+        Set<BusRouteStop> busRouteStops = Collections.emptySet();
+
+        //when
+        BusRouteStop firstStopOnTheRoute = BusStopTools.findFirstStopOnTheRoute(busRouteStops);
+
+        //then
+        assertThat(firstStopOnTheRoute, nullValue());
+    }
+
+    @Test
+    void busStopWithLowestNumberShouldBeReturnedFromFindFirstStopOnTheRouteWhenRouteStopCollectionHaveManyElementsWithDifferentNumbers() {
+        //given
+        Set<BusRouteStop> busRouteStops = getBusRouteStopsWithNumberFromOneToFour();
+        BusRouteStop firstBusStop = busRouteStops.stream().filter(bs -> bs.getNumber() == 1).findFirst().get();
+
+        //when
+        BusRouteStop firstStopOnTheRoute = BusStopTools.findFirstStopOnTheRoute(busRouteStops);
+
+        //then
+        assertThat(firstStopOnTheRoute, notNullValue());
+        assertThat(firstStopOnTheRoute.getNumber(), equalTo(1));
+        assertThat(firstStopOnTheRoute, sameInstance(firstBusStop));
+    }
+
+    @Test
+    void nullPointerExceptionShouldBeThrownFromFindFirstStopOnTheRouteWhenCollectionOfBusRouteStopsAreNull(){
+        //given
+        //when
+        //then
+        assertThrows(NullPointerException.class, () -> BusStopTools.findFirstStopOnTheRoute(null));
+    }
+
+    @Test
+    void nullPointerExceptionShouldBeThrownFromFindLastStopOnTheRouteWhenCollectionOfBusRouteStopsAreNull(){
+        //given
+        //when
+        //then
+        assertThrows(NullPointerException.class, () -> BusStopTools.findLastStopOnTheRoute(null));
+    }
+
+    @Test
+    void busStopWithHighestNumberShouldBeReturnedFromFindFirstStopOnTheRouteWhenRouteStopCollectionHaveManyElementsWithDifferentNumbers() {
+        //given
+        Set<BusRouteStop> busRouteStops = getBusRouteStopsWithNumberFromOneToFour();
+        BusRouteStop firstBusStop = busRouteStops.stream().filter(bs -> bs.getNumber() == 4).findFirst().get();
+
+        //when
+        BusRouteStop firstStopOnTheRoute = BusStopTools.findLastStopOnTheRoute(busRouteStops);
+
+        //then
+        assertThat(firstStopOnTheRoute, notNullValue());
+        assertThat(firstStopOnTheRoute.getNumber(), equalTo(4));
+        assertThat(firstStopOnTheRoute, sameInstance(firstBusStop));
+    }
+
+    private static Set<BusRouteStop> getBusRouteStopsWithNumberFromOneToFour() {
+        return Set.of(
+                new BusRouteStop(1, null, null, null, null),
+                new BusRouteStop(2, null, null, null, null),
+                new BusRouteStop(3, null, null, null, null),
+                new BusRouteStop(4, null, null, null, null)
+        );
+    }
+
+    @Test
+    void nullShouldBeReturnedFromFindLastStopOnTheRouteWhenRouteStopCollectionIsEmpty() {
+        //given
+        Set<BusRouteStop> busRouteStops = Collections.emptySet();
+
+        //when
+        BusRouteStop firstStopOnTheRoute = BusStopTools.findFirstStopOnTheRoute(busRouteStops);
+
+        //then
+        assertThat(firstStopOnTheRoute, nullValue());
+    }
+}
